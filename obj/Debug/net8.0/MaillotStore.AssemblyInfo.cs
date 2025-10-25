41ed846815dc8521c6ff5425ae814d71a09caf5b
CHUNELSON123 <chubehnelson@gmail.com>
2025-10-25 11:43:56 +0100

    finaltouches

--- obj/Debug/net8.0/MaillotStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/MaillotStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaillotStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c597e67b46718e49fefdd308f4155cdeef64c593")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c70182efa8e345554b88501be143f597db65d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaillotStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaillotStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
